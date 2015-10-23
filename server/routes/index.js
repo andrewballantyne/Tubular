877066f6ec3f0fd7be39d695fccaae7d20b58b0d
Daniel Bruce <dbruce1126@gmail.com>
2015-10-22 22:18:11 -0400

    OAuth authorization URL and params.js.dist.

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {}); 
+  var authorizationUri = 'https://accounts.google.com/o/oauth2/auth?' +
+    'client_id=' + encodeURIComponent(req.app.locals.params.oauth.client_id) + '&' +
+    'redirect_uri=' + encodeURIComponent(req.app.locals.params.oauth.redirect_uri) + '&' +
+    'response_type=' + 'code' + '&' +
+    'scope=' + encodeURIComponent('https://www.googleapis.com/auth/youtube.readonly');
+  res.render('index', {
+    params: req.app.locals.params,
+    authorizationUri: authorizationUri
+  });
 });
 
 module.exports = router;
